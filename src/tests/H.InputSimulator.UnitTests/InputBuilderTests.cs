2b8153c79c9017734df80e3f0563e8def8eced5d
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-26 16:13:51 +1000

    test: Moved InputBuilderTests to UnitTests.

---
+++ src/tests/H.InputSimulator.UnitTests/InputBuilderTests.cs
@@ -0,0 +1,17 @@
+﻿namespace WindowsInput.Native.Tests;
+
+[TestClass]
+public class InputBuilderTests
+{
+    [TestMethod]
+    public void AddKeyDown()
+    {
+        var builder = new InputBuilder();
+        builder.ToArray().Should().BeEmpty();
+
+        builder.AddKeyDown(VirtualKeyCode.VK_A);
+        builder.Count.Should().Be(1);
+        builder[0].Type.Should().Be((uint)InputType.Keyboard);
+        builder[0].Data.Keyboard.KeyCode.Should().Be((ushort)VirtualKeyCode.VK_A);
+    }
+}
