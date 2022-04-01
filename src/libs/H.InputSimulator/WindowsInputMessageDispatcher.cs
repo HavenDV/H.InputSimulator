68fa641bb9f3b444bec1e097ee9f6cfad21fd85d
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-04-01 14:15:46 +1000

    refactor: Refactored.

--- src/libs/H.InputSimulator/WindowsInputMessageDispatcher.cs
+++ src/libs/H.InputSimulator/WindowsInputMessageDispatcher.cs
@@ -20,13 +20,29 @@ internal class WindowsInputMessageDispatcher : IInputMessageDispatcher
     /// <exception cref="Exception">If the any of the commands in the <paramref name="inputs"/> array could not be sent successfully.</exception>
     public unsafe void DispatchInput(INPUT[] inputs)
     {
-        if (inputs == null) throw new ArgumentNullException(nameof(inputs));
-        if (inputs.Length == 0) throw new ArgumentException("The input array was empty", nameof(inputs));
-        fixed(INPUT* inputsPtr = inputs)
+        if (inputs == null)
+        {
+            throw new ArgumentNullException(nameof(inputs));
+        }
+        if (inputs.Length == 0)
+        {
+            throw new ArgumentException("The input array was empty", nameof(inputs));
+        }
+
+        fixed (INPUT* inputsPtr = inputs)
         {
             var successful = PInvoke.SendInput((uint)inputs.Length, inputsPtr, sizeof(INPUT));
             if (successful != inputs.Length)
-                throw new InvalidOperationException("Some simulated input commands were not sent successfully. The most common reason for this happening are the security features of Windows including User Interface Privacy Isolation (UIPI). Your application can only send commands to applications of the same or lower elevation. Similarly certain commands are restricted to Accessibility/UIAutomation applications. Refer to the project home page and the code samples for more information.");
+            {
+                throw new InvalidOperationException(
+                    "Some simulated input commands were not sent successfully. " +
+                    "The most common reason for this happening are the security features of " +
+                    "Windows including User Interface Privacy Isolation (UIPI). " +
+                    "Your application can only send commands to applications of " +
+                    "the same or lower elevation. Similarly certain commands are restricted to " +
+                    "Accessibility/UIAutomation applications. " +
+                    "Refer to the project home page and the code samples for more information.");
+            }
         }
     }
 }
